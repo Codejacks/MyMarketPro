9459f8f85666e8deaca8aff4efdf5f97975f7fcc
chenlouis <619189810@qq.com>
2015-10-06 09:48:21 +0800

    add DefaultAdapter

---
+++ myframework/src/main/java/me/chenfuduo/myframework/DefaultAdapter.java
@@ -0,0 +1,50 @@
+package me.chenfuduo.myframework;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+/**
+ * Created by chenfuduo on 2015/10/4.
+ */
+public abstract class DefaultAdapter<T> extends BaseAdapter {
+
+    private List<T> datas;
+
+    public DefaultAdapter(List<T> datas) {
+        this.datas = datas;
+    }
+
+    @Override
+    public int getCount() {
+        return datas.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return datas.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        BaseHolder mainHolder;
+        if (convertView == null) {
+            mainHolder = getHolder();
+        } else {
+            mainHolder = (BaseHolder) convertView.getTag();
+        }
+        T str = datas.get(position);
+        mainHolder.setData(str);
+        return mainHolder.getConvertView();
+    }
+
+    public abstract BaseHolder getHolder();
+}
